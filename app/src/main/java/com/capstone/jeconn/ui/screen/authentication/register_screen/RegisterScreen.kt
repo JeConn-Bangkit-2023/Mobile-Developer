a56349892677144b1e92e133f60a03a4a92cab19
fauzangraciaramadhani <fauzanramadhani06@gmail.com>
2023-05-25 11:51:10 +0700

    feat: optimize code for bottom navigation

--- app/src/main/java/com/capstone/jeconn/ui/screen/authentication/register_screen/RegisterScreen.kt
+++ app/src/main/java/com/capstone/jeconn/ui/screen/authentication/register_screen/RegisterScreen.kt
@@ -1,9 +1,35 @@
 package com.capstone.jeconn.ui.screen.authentication.register_screen
 
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.capstone.jeconn.R
+import com.capstone.jeconn.component.CustomButton
+import com.capstone.jeconn.navigation.NavRoute
+import com.capstone.jeconn.utils.popUpToTop
 
 @Composable
 fun RegisterScreen(navHostController: NavHostController) {
-    //TODO
+    Log.e("RegisterScreen", "test")
+    val context = LocalContext.current
+    //Login Button
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        CustomButton(
+            text = context.getString(R.string.login),
+            modifier = Modifier
+                .padding(vertical = 24.dp)
+        ) {
+            navHostController.navigate(NavRoute.Dashboard.route) {
+                popUpToTop(navHostController)
+            }
+        }
+    }
 }
--- app/src/main/java/com/capstone/jeconn/ui/screen/dashboard/BaseScreen.kt
+++ app/src/main/java/com/capstone/jeconn/ui/screen/dashboard/BaseScreen.kt
@@ -1,154 +1,126 @@
 package com.capstone.jeconn.ui.screen.dashboard
 
-import androidx.compose.foundation.background
+import android.annotation.SuppressLint
+import android.app.Activity
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import com.capstone.jeconn.R
 import com.capstone.jeconn.component.Font
-import com.capstone.jeconn.navigation.NavRoute
+import com.capstone.jeconn.ui.screen.dashboard.freelancer_screen.FreelancerScreen
+import com.capstone.jeconn.ui.screen.dashboard.home_screen.HomeScreen
+import com.capstone.jeconn.ui.screen.dashboard.profile_screen.ProfileScreen
+import com.capstone.jeconn.ui.screen.dashboard.status_screen.StatusScreen
+import com.capstone.jeconn.ui.screen.dashboard.vacancies_screen.VacanciesScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-//Test
 fun BaseScreen(
     navHostController: NavHostController,
-    content: @Composable (PaddingValues) -> Unit = {}
 ) {
-    Scaffold(
-        bottomBar = {
-            BottomBar(navHostController)
-        }
-    ) { paddingValues ->
-        content.invoke(paddingValues)
-    }
-}
-
-@Composable
-fun BottomBar(navController: NavController) {
     val context = LocalContext.current
+    var contentRoute by remember {
+        mutableStateOf(0)
+    }
+
+    BackHandler {
+        if (contentRoute == 0) {
+            (context as Activity).finish()
+        } else {
+            contentRoute = 0
+        }
+    }
 
     val screens = listOf(
         DashboardContent(
-            screen = NavRoute.HomeScreen,
             title = context.getString(R.string.home),
-            icon = R.drawable.ic_home
+            icon = R.drawable.ic_home,
+            content = { HomeScreen(navHostController = navHostController) }
         ),
         DashboardContent(
-            screen = NavRoute.VacanciesScreen,
             title = context.getString(R.string.vacancies),
-            icon = R.drawable.ic_vacancies
+            icon = R.drawable.ic_vacancies,
+            content = { VacanciesScreen(navHostController = navHostController) }
         ),
         DashboardContent(
-            screen = NavRoute.FreelancerScreen,
             title = context.getString(R.string.freelance),
-            icon = R.drawable.ic_freelancer
+            icon = R.drawable.ic_freelancer,
+            content = { FreelancerScreen(navHostController = navHostController) }
         ),
         DashboardContent(
-            screen = NavRoute.StatusScreen,
             title = context.getString(R.string.status),
-            icon = R.drawable.ic_status
+            icon = R.drawable.ic_status,
+            content = { StatusScreen(navHostController = navHostController) }
         ),
         DashboardContent(
-            screen = NavRoute.ProfileScreen,
             title = context.getString(R.string.profile),
-            icon = R.drawable.ic_profile
+            icon = R.drawable.ic_profile,
+            content = { ProfileScreen(navHostController = navHostController) }
         ),
     )
 
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentDestination = navBackStackEntry?.destination
-    val bottomDestination = screens.any { it.screen.route == currentDestination?.route }
-    if (bottomDestination) {
-        BottomNavigation(
-            backgroundColor = MaterialTheme.colorScheme.onPrimary
-        ) {
-            screens.forEach { screen ->
-                AddItem(
-                    screen = screen,
-                    currentDestination = currentDestination,
-                    navController = navController
-                )
+    Scaffold(
+        bottomBar = {
+            NavigationBar(containerColor = MaterialTheme.colorScheme.onPrimary) {
+                screens.forEachIndexed { index, item ->
+                    val selected = contentRoute == index
+
+                    val iconSelectedColor =
+                        if (selected) MaterialTheme.colorScheme.inversePrimary else MaterialTheme.colorScheme.onBackground
+                    val fontWeight = if (selected) FontWeight.Bold else FontWeight.Normal
+
+                    NavigationBarItem(
+                        selected = selected,
+                        colors = NavigationBarItemDefaults.colors(
+                            indicatorColor = MaterialTheme.colorScheme.secondary
+                        ),
+                        onClick = {
+                            contentRoute = index
+                        },
+                        label = {
+                            Text(
+                                text = item.title,
+                                fontSize = 10.sp,
+                                fontFamily = Font.QuickSand,
+                                fontWeight = fontWeight,
+                            )
+                        },
+                        icon = {
+                            Icon(
+                                painterResource(item.icon),
+                                tint = iconSelectedColor,
+                                contentDescription = null,
+                            )
+                        }
+                    )
+                }
             }
         }
+    ) { paddingValues ->
+        screens[contentRoute].content.invoke(paddingValues)
     }
 }
 
 data class DashboardContent(
-    val screen: NavRoute,
     val title: String,
-    val icon: Int
+    val icon: Int,
+    val content: @Composable (PaddingValues) -> Unit = {}
 )
-
-@Composable
-fun RowScope.AddItem(
-    screen: DashboardContent,
-    currentDestination: NavDestination?,
-    navController: NavController
-) {
-    val isSelected = rememberSaveable {
-        mutableStateOf(
-            currentDestination?.hierarchy?.any {
-                it.route == screen.screen.route
-            } == true
-        )
-    }
-    val backgroundColor =
-        if (isSelected.value) MaterialTheme.colorScheme.secondary else MaterialTheme.colorScheme.onPrimary
-    val clip = if (isSelected.value) RoundedCornerShape(25.dp) else RoundedCornerShape(0.dp)
-    val selectedColor =
-        if (isSelected.value) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSurface
-    BottomNavigationItem(
-        label = {
-            Text(
-                text = screen.title,
-                color = selectedColor,
-                fontSize = 10.sp,
-                fontFamily = Font.QuickSand,
-                fontWeight = FontWeight.Medium
-            )
-        },
-        icon = {
-            Icon(
-                painterResource(screen.icon),
-                contentDescription = null
-            )
-        },
-        selected = isSelected.value,
-        modifier = Modifier
-            .clip(clip)
-            .background(backgroundColor),
-        selectedContentColor = MaterialTheme.colorScheme.onPrimary,
-        unselectedContentColor = Color(0x30000000),
-        onClick = {
-            navController.navigate(screen.screen.route) {
-                popUpTo(NavRoute.HomeScreen.route)
-                launchSingleTop = true
-            }
-        }
-    )
-}
