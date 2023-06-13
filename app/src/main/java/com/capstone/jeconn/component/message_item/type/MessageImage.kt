e6b59754db42b1f66da7e14371993df427f93936
fauzangraciaramadhani <fauzanramadhani06@gmail.com>
2023-06-13 19:49:59 +0700

    fix: chat UI

--- app/src/main/java/com/capstone/jeconn/component/message_item/type/MessageImage.kt
+++ app/src/main/java/com/capstone/jeconn/component/message_item/type/MessageImage.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -45,6 +46,11 @@ fun MessageImage(
 ) {
     val context = LocalContext.current
     val showDialog = remember { mutableStateOf(false) }
+    val paddingValues = if (isMine) {
+        PaddingValues(start = 128.dp)
+    } else {
+        PaddingValues(end = 64.dp)
+    }
 
     Row(
         verticalAlignment = Alignment.Bottom,
@@ -55,6 +61,7 @@ fun MessageImage(
         },
         modifier = Modifier
             .fillMaxWidth()
+            .padding(paddingValues)
     ) {
         if (!isMine) {
             CropToSquareImage(
